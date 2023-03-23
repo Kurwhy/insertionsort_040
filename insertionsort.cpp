509035b3ada06b1d09b85d20af27a468bdb3be8d
Wahyu <firmansyahw503@gmail.com>
2023-03-23 11:22:10 +0700

    Membuat tampilan

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -54,5 +54,19 @@ void insertionsort(){			// Procedure Tnsertionsort
 		for (int k = 0; k < n; k++) {		// Looping nilai k dimulai dari 0 hingga n-1
 			cout << arr[k] << " ";			// Output ke layar
 		}
+
 	}
-}		
+}
+
+void display() {			// Procedure Display
+	cout << endl;												// Output baris kosong
+	cout << "\n==================================" << endl;		// Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;		// Output ke layar
+	cout << "==================================" << endl;		// Output ke layar
+
+	for (int j = 0; j < n; j++) {					// Looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;						// Output ke layar
+	}
+	cout << endl;									// Output baris kosong 
+}
+
