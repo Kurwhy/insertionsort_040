4bad1d02c6c7523a7dd24cf4b863bdfde16b3ec6
Wahyu <firmansyahw503@gmail.com>
2023-03-23 10:22:56 +0700

    baris 7 - 20

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -4,3 +4,17 @@ using namespace std;
 int arr[20];												// Membuat Array dengan panjang data 20
 int n;														// Membuat Variable imputan n
 
+void input() {							// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : "; // Membuat Imputan jumlah element Array
+		cin >> n;									  // memanggil variable inputan n
+
+		if (n <= 20) {								  // Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
+		}
+	}
